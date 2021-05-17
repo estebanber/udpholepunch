927300a94bd06b585b5a92d2bf86a72bb68459b7
Esteban Berná <estebanberna@gmail.com>
2021-05-17 11:17:37 -0300

    Mejoras

--- udpClient.py
+++ udpClient.py
@@ -10,18 +10,42 @@ addresses = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
 def mainReciever(host='127.0.0.1', port=9999):
-    sock.sendto(b'0', (host, port))
     while True:
-        data=''
-        data, addr = sock.recvfrom(1024)
-        print(data)
-        pck = data[0:4].decode('utf-8')
-        if pck == 'LIST':
-            print('list received from {}\n{}'.format(addr, data.decode('utf-8')))
-            actualizarLista(addresses,data)
-        elif pck == 'NEWC':
+        comm = input('Enter Command > ')
+        if comm == 'LIST':
+            sock.sendto(b'LIST', (host, port))
+            data, addr = sock.recvfrom(1024)
+            res = data[0:4].decode('utf-8')
+            if res == 'LIST':
+                print('List received from {}\n{}'.format(addr, data.decode('utf-8')))
+                actualizarLista(addresses,data)
+        elif comm[0:4] == 'CONN':
+            sock.sendto(comm.encode('utf-8'), (host,port))
+            data, addr = sock.recvfrom(1024)
+            print(data)
             addr = msg_to_addr(data)
-            sock.sendto(b'0', addr)
+            print(addr)
+            hsok=False
+            hnsk=False
+            connReady=False
+            while not connReady:
+                sock.sendto(b'HNSK', addr)
+                data, addr = sock.recvfrom(1024)
+                print(data)
+                if data==b'HNSK':
+                    sock.sendto(b'HSOK', addr)
+                    hnsk=True;
+                elif data==b'HSOK':
+                    hsok=True
+                if hnsk and hsok:
+                    print('Ready')
+                    connReady = True
+            sleep(1)
+            print(f'Asking streaming to {addr}')
+            sock.sendto(b'STRM', addr)
+            while True:
+                data, addr = sock.recvfrom(1024)
+                print(data)
 
 def actualizarLista(lista,data):
     listaString = data.decode('utf-8')
@@ -33,45 +57,53 @@ def actualizarLista(lista,data):
     print(lista)
 
 
-def getCommand():
-    while True:
-        comm = input('Select client> ')
-        if comm == 'N':
-            continue
-        elif comm == 'S':
-            comm = input('Select client> ')
-            addr= addresses[comm]
-            sock.sendto(b'STOP', addr)
-        else:
-            addr= addresses[comm]
-            sock.sendto(b'STRM', addr)
-
-
-
 
 def mainSender(host='127.0.0.1', port=9999):
     sock.sendto(b'0', (host, port))
     while True:
         data, addr = sock.recvfrom(1024)
-        pck = data[0:4].decode('utf-8')
+        com = data[0:4].decode('utf-8')
+        print(len(data))
         print(data)
-        if pck == 'NEWC':
+        if com == 'HNSK':
+            sock.sendto(b'HSOK', addr)
+
+        if com == 'NEWC':
+            data, addr = sock.recvfrom(1024)
+            print(data)
             addr = msg_to_addr(data)
-            sock.sendto(b'0', addr)
-        elif pck == 'STRM': #Activate streaming
+            print(addr)
+            hsok=False
+            hnsk=False
+            connReady=False
+            while not connReady:
+                sock.sendto(b'HNSK', addr)
+                data, addr = sock.recvfrom(1024)
+                print(data)
+                if data==b'HNSK':
+                    sock.sendto(b'HSOK', addr)
+                    hnsk=True;
+                elif data==b'HSOK':
+                    hsok=True
+                if hnsk and hsok:
+                    print('Ready')
+                    connReady = True
+            sleep(1)
+            data, addr = sock.recvfrom(1024)
+            com = data[0:4].decode('utf-8')
+            print(com)
+        if com == 'STRM': #Activate streaming
             count=0;
             while True:
                 sleep(1)
-                print('Sending to {addr}: Dato {count}')
-                sock.sendto(bytes('Dato {count}\n','utf-8'), addr)
+                print(f'Sending to {addr}: Dato {count}')
+                sock.sendto(bytes(f'Dato {count}\n','utf-8'), addr)
                 count=count +1
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
     if sys.argv[1] == 'R':
-        hilo = threading.Thread(target=getCommand)
-        hilo.start()
         argumentos= sys.argv[1:4]
         mainReciever(*addr_from_args(argumentos))
     elif sys.argv[1] == 'S':
--- udpServer.py
+++ udpServer.py
@@ -14,13 +14,21 @@ def main(host='0.0.0.0', port=9999):
     logger.info("server - waiting connections in: %s:%s", host,port)
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        sock.sendto(b'OKOK', addr)
-        logger.info("connection from: %s - %s", addr, data)
-        addresses.append(addr)
-        for a in addresses:
-            sock.sendto(b'LIST\n'+list_of_addresses(addresses), a)
-
-
+        if data == b'0':
+            sock.sendto(b'OK', addr)
+            logger.info("connection from: %s - %s", addr, data)
+            addresses.append(addr)
+        elif data[0:4] == b'CONN':
+            command= data.decode('utf-8')
+            print(int(command[4]))
+            print(addresses)
+            sock.sendto(addr_to_msg(addresses[int(command[4])]), addr)
+            sock.sendto(b'NEWC', addresses[int(command[4])])
+            sock.sendto(addr_to_msg(addr), addresses[int(command[4])])
+        elif data == b'LIST':
+            sock.sendto(b'LIST\n'+list_of_addresses(addresses), addr)
+            
+        
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
     main(*addr_from_args(sys.argv))
